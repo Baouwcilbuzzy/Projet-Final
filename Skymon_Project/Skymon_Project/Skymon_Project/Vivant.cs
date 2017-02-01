5a0a8b9ab3a01344f445b7064609602900392b34
Antoi-Toine <Antoi-Toine@ANTOINE-PC>
2017-02-01 11:40:27 -0500

    Yo on a fait une partie du projet : la classe vivant

---
+++ Skymon_Project/Skymon_Project/Skymon_Project/Vivant.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Skymon_Project
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public abstract class Vivant : Microsoft.Xna.Framework.GameComponent
+    {
+        protected string Nom { get; private set; }
+        public Vivant(Game game, string nom)
+            : base(game)
+        {
+            Nom = nom;
+            // TODO: Construct any child components here
+        }
+        public bool EstEnVie()
+        {
+            foreach (Pokemon in Pokemons/*liste de pokemons que le trainer a !!!!!!*/)
+                if (!(Pokemon.EstEnVie))
+                {
+                    return false;
+                }
+            return true;
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
